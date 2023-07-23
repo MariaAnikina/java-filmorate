7c79836fc633e4f7399f485bcfffa537ee9580ec
Мария <mashunia0306@yandex.ru>
2023-07-23 18:14:49 +0300

    9 c

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -33,7 +33,7 @@ public class FilmController {
             log.warn("Возникло исключение - попытка создать существующий фильм: {}", film);
             throw new ValidationException("Такой фильм уже существует.");
         }
-        if (film.getName().isBlank()){
+        if (film.getName().isBlank()) {
             log.warn("Возникло исключение - попытка создать фильм без названия: {}", film);
             throw new ValidationException("У фильма должно быть название.");
         }
@@ -41,7 +41,7 @@ public class FilmController {
             log.warn("Возникло исключение - попытка создать фильм с описанием длиною более 200 символов: {}", film);
             throw new ValidationException("Описание бильма должно быть длигною менее 201 символа");
         }
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))){
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
             log.warn("Возникло исключение - попытка создать фильм с неккоректной датой выпуска: {}", film);
             throw new ValidationException("В то время фильмов не было(");
         }
